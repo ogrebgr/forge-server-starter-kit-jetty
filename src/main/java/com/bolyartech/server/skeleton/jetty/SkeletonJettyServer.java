5a38cb620b2ef488f9eab4e6a426911418ae3ed6
Ognyan Bankov <ogibankov@gmail.com>
2017-01-10 14:54:15 +0200

    initial import

---
+++ src/main/java/com/bolyartech/server/skeleton/jetty/SkeletonJettyServer.java
@@ -0,0 +1,28 @@
+package com.bolyartech.server.skeleton.jetty;
+
+import com.bolyartech.forge.server.SkeletonMainServlet;
+import com.bolyartech.forge.server.jetty.ForgeJetty;
+import com.bolyartech.forge.server.jetty.ForgeJettyConfigurationLoader;
+import com.bolyartech.forge.server.jetty.ForgeJettyConfigurationLoaderImpl;
+
+import javax.servlet.http.HttpServlet;
+
+
+public class SkeletonJettyServer extends ForgeJetty {
+
+    public SkeletonJettyServer(ForgeJettyConfigurationLoader forgeJettyConfigurationLoader) {
+        super(forgeJettyConfigurationLoader);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        SkeletonJettyServer server = new SkeletonJettyServer(new ForgeJettyConfigurationLoaderImpl());
+        server.start();
+    }
+
+    public HttpServlet createMainServlet() {
+        return new SkeletonMainServlet();
+    }
+}
