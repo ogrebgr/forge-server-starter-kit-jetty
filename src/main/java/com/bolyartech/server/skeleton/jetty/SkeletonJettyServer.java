6ebbab52632d2f0d65f5cd0749e1da702841f99d
Ognyan Bankov <ogibankov@gmail.com>
2017-02-02 17:35:45 +0200

    Now using conf/ dir outside resources dir

--- src/main/java/com/bolyartech/server/skeleton/jetty/SkeletonJettyServer.java
+++ src/main/java/com/bolyartech/server/skeleton/jetty/SkeletonJettyServer.java
@@ -3,7 +3,8 @@ package com.bolyartech.server.skeleton.jetty;
 import com.bolyartech.forge.server.SkeletonMainServlet;
 import com.bolyartech.forge.server.jetty.ForgeJetty;
 import com.bolyartech.forge.server.jetty.ForgeJettyConfigurationLoader;
-import com.bolyartech.forge.server.jetty.ForgeJettyConfigurationLoaderImpl;
+import com.bolyartech.forge.server.jetty.ForgeJettyConfigurationLoaderClasspath;
+import com.bolyartech.forge.server.jetty.ForgeJettyConfigurationLoaderFile;
 
 import javax.servlet.http.HttpServlet;
 
@@ -16,7 +17,9 @@ public class SkeletonJettyServer extends ForgeJetty {
 
 
     public static void main(String[] args) {
-        SkeletonJettyServer server = new SkeletonJettyServer(new ForgeJettyConfigurationLoaderImpl());
+        SkeletonJettyServer server = new SkeletonJettyServer(
+                new ForgeJettyConfigurationLoaderFile(ForgeJettyConfigurationLoaderFile.detectJettyConfigFilePath()));
+
         server.start();
     }
 
